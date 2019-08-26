d076f8a6368c434e2f59e3675c143b882d704a0a
Suyesh Bhandari <suyeshb@gmail.com>
2019-08-26 17:25:40 -0400

    Test setup

---
+++ src/components/CarouselSlide.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const CarouselSlide = props => (
+  <figure>
+    <img src={props.imgUrl} />
+    <figcaption>
+      <strong>{props.description}</strong> {props.attribution}
+    </figcaption>
+  </figure>
+);
+
+CarouselSlide.propTypes = {
+  imgUrl: PropTypes.string.isRequired,
+  description: PropTypes.string.isRequired,
+  attribution: PropTypes.string.isRequired
+};
+
+export default CarouselSlide;
---
+++ src/components/__tests__/CarouselSlide.test.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import CarouselSlide from '../CarouselSlide';
+
+describe('CarouselSlide', () => {
+  let wrapper;
+
+  beforeEach(() => {
+    wrapper = shallow(<CarouselSlide imgUrl="default" description="default" attribution="default" />);
+  });
+
+  it('renders a <figure/>', () => {
+    expect(wrapper.type()).toBe('figure');
+  });
+
+  it('renders an <img> and a <figcaption> as children', () => {
+    expect(wrapper.childAt(0).type()).toBe('img');
+    expect(wrapper.childAt(1).type()).toBe('figcaption');
+  });
+
+  it('passes `imgUrl` through to the <img>', () => {
+    const imgUrl = 'https://example.com/image.png';
+    wrapper.setProps({ imgUrl });
+    const img = wrapper.find('img');
+    expect(img.prop('src')).toBe(imgUrl);
+  });
+
+  it('uses `description` and `attribution` as the <figcaption></figcaption>', () => {
+    const description = 'A jaw droppingly spectacular image';
+    const attribution = 'Trevor Burnham';
+    wrapper.setProps({ description, attribution });
+    expect(wrapper.find('figcaption').text()).toBe(`${description} ${attribution}`);
+    expect(wrapper.find('figcaption strong').text()).toBe(description);
+  });
+});
